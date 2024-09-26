e9ee613c57db13fc909bb54bf4fe9fcdbf5af660
Nathan <Nathanhsiao123@gmail.com>
2024-09-25 20:09:48 -0500

    new

--- extract/parse.py
+++ extract/parse.py
@@ -22,18 +22,35 @@ job_description = (
     "with experience in building responsive web applications and collaborating with UX/UI teams."
 )
 
-res = (
-    """•Collaborated on the development of a management system which reduced
-Proactive approach led to the development of
-bug fix times by 20%.
-a new tracking system for tutoring
-• Assisted in the maintenance and improvement of company codebase,
-appointments, increasing efficiency by 35%.
-leading to a 15% increase in efficiency.
-• Utilized analytical skills in solving technical issues, resulting in a 30%
-reduction in client complaints.""")
+job = (
+   """Responsibilities:
+  6-10 years of experience in Financial Services preferred
+ Global Project Management experience preferred
+ Ability to work under pressure and manage deadlines or unexpected changes in expectations or requirements
+ Proven organizational and time management skills""")
 
-result = edit.tailor_resume(res, job_description)
+res = (
+    """A finance professional, I am deeply passionate about understanding
+macro as well as the micro aspects affecting corporate finances.
+I have engaged in providing equity fund raising, debt syndication,
+M&A and investor relations services to 50+ companies across
+sectors including Healthcare, Life Sciences, Energy, Industrials,
+Consumer and Technology.
+My 6+ years of experience across Investor Relations, Investment
+Banking and Corporate Finance Advisory helped me develop
+competencies including business storytelling, which is my key
+professional interest area, and problem-solving, which energizes me
+emotionally.
+An MBA from UNC Kenan-Flagler Business School (Ranked 19th
+Best Business School, US News, 2018), I have extensive experience
+in business and strategy analysis, financial modelling, stock price
+analysis, team management, investor communication and event
+management, and pitch material presentation. 
+I believe that knowledge, ability to communicate effectively, humility
+and integrity are the primary ingredients of, what truly is, the net
+worth of an individual.  """)
+
+result = edit.tailor_resume(res, job)
 
 #print(' ')
 print(result['response'])
