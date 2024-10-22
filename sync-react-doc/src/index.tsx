4880797ad35145d9c710ecd2f582f7c9ca9a9ffb
Nathan Hsiao <Nathanhsiao123@gmail.com>
2024-10-22 18:31:33 -0500

    Read package template code for sync library

---
+++ sync-react-doc/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("MzU0MzQ1OUAzMjMwMmUzNDJlMzBoMUhlUWpCZnV3OU1BT0c2U3FDT2IzOVZWU2tSc24yejhiNmdQUGVhODNzPQ==;MzU0MzQ2MEAzMjMwMmUzNDJlMzBadmNsWGtVTmlSbENWcjZITDdpazZuN2JLR3V5eUtqTDVuRUZXOFBnQ2gwPQ==;Mgo+DSMBaFt/QHRqVVhlWFpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQFliSXxRd0xjXH9WcX1VRw==;Mgo+DSMBPh8sVXJ0S0J+XE9BclRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3pSdkdhWHZedXRdRGBZVw==;ORg4AjUWIQA/Gnt2VVhkQlFac1tJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxXd0VhW35XcHFVTmJcVEI=;NRAiBiAaIQQuGjN/V0Z+WE9EaFpGVmJLYVB3WmpQdldgdVRMZVVbQX9PIiBoS35RckRjW31feXRQRmheU0V0;MzU0MzQ2NUAzMjMwMmUzNDJlMzBZY3A5bHBYTi9PNXBOMlRDd09kNUVTcjh2bjlSUXZVOHFLanppTVphUnM4PQ==;MzU0MzQ2NkAzMjMwMmUzNDJlMzBkb01XblpMN0xOSFEyMHpoSFh4OWgyRDJ2V1lXQVNyMHdVYWdxRjNuOFZZPQ==;Mgo+DSMBMAY9C3t2VVhkQlFac1tJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxXd0VhW35XcHFVTmJaWUU=;MzU0MzQ2OEAzMjMwMmUzNDJlMzBIOFNUdkhVMWJyWkZPbHpYZmVONlNDQjFIR09qckNpUFQ0amZPODJXa2tRPQ==;MzU0MzQ2OUAzMjMwMmUzNDJlMzBkK0ZDUTJNdkkxT2dQcEY0cnh4cGVOTGk1QzhZVEhSSTBDN1drUVZMbzlZPQ==;MzU0MzQ3MEAzMjMwMmUzNDJlMzBZY3A5bHBYTi9PNXBOMlRDd09kNUVTcjh2bjlSUXZVOHFLanppTVphUnM4PQ==")
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
