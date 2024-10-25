7d8ed7cdebdf7cfa7f86582db7ea8b88fe40386c
Nathan Hsiao <Nathanhsiao123@gmail.com>
2024-10-24 19:07:27 -0500

    License key for sync fusion package

--- angular-editor/src/main.ts
+++ angular-editor/src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
 
-registerLicense('Enter your license key');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF1cXGFCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH9cdHZVRmRdVE1yWUI=');
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
